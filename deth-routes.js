afd7adcceee553157c50b1b91bb6ea04eac721c8
Robert Helmer <rhelmer@rhelmer.org>
2016-07-11 16:26:50 -0700

    use new class syntax

--- deth-routes.js
+++ deth-routes.js
@@ -1,8 +1,5 @@
 "use strict";
 
-const ZONEFILE = './zones/zone.txt';
-const RTYPES = ['ns', 'a', 'aaaa', 'srv', 'txt'];
-
 let express = require('express');
 let errors = require('express-api-server').errors;
 let jsonParser = require('body-parser').json();
@@ -11,75 +8,80 @@ let fs = require('fs');
 
 let router = module.exports = express.Router();
 
-// cached zone data, TODO maybe put this in a function
-let zoneTxt = fs.readFileSync(ZONEFILE, 'utf8');
-let cachedZone = zonefile.parse(zoneTxt);
+class Zone {
+  constructor() {
+    this.zoneFile = './zones/zone.txt';
+    this.rtypes = ['ns', 'a', 'aaaa', 'srv', 'txt'];
 
-/**
-  * Generate a spec-compliant output object from a full zone object.
-  *
-  * @param zone - full zone object.
-  * @returns spec-compliant output object, suitable for returning to client
-  *          as JSON.
-  */
-function generateOutput(zone) {
-  let output = {};
-
-  RTYPES.map(t => {
-    if (t in cachedZone) {
-      output[t] = cachedZone[t];
+    let zoneTxt = fs.readFileSync(this.zoneFile, 'utf8');
+    this.cachedZone = zonefile.parse(zoneTxt);
+  }
+  /**
+    * Modify a zone with new input, save to disk, and reload the DNS server.
+    *
+    * @param newZone - the new zone object to merge into the original.
+    * @returns new zone object.
+    */
+  modify(newZone) {
+    // empty objects aren't useful.
+    if (Object.getOwnPropertyNames(newZone).length == 0) {
+      return this.cachedZone;
     }
-  });
 
-  return output;
+    // save new zone file to disk
+    let newRecordTxt = zonefile.generate(newZone);
+    let newRecord = zonefile.parse(newRecordTxt);
+    fs.writeFileSync(this.zoneFile, newRecordTxt, 'utf8');
+
+    // re-load cache
+    zoneTxt = fs.readFileSync(this.zoneFile, 'utf8');
+    this.cachedZone = zonefile.parse(zoneTxt);
+
+    // TODO reload DNS server
+    return newZone;
+  }
+
+  /**
+    * Generate a spec-compliant output object from a full zone object.
+    *
+    * @returns spec-compliant output object, suitable for returning to client
+    *          as JSON.
+    */
+  get generateOutput() {
+    let output = {};
+
+    this.rtypes.map(t => {
+      if (t in this.cachedZone) {
+        output[t] = this.cachedZone[t];
+      }
+    });
+
+    return output;
+  }
 }
 
-/**
-  * Modify a zone with new input, save to disk, and reload the DNS server.
-  *
-  * @param oldZone - the original zone object.
-  * @param newZone - the new zone object to merge into the original.
-  * @returns new zone object.
-  */
-function modifyZone(oldZone, newZone) {
-  // TODO validate input and modify zone based on input
-  newZone = oldZone;
-
-  // save new zone file to disk
-  let newRecordTxt = zonefile.generate(newZone);
-  let newRecord = zonefile.parse(newRecordTxt);
-  fs.writeFileSync(ZONEFILE, newRecordTxt, 'utf8');
-
-  // re-load cache
-  zoneTxt = fs.readFileSync(ZONEFILE, 'utf8');
-  cachedZone = zonefile.parse(zoneTxt);
-
-  // TODO reload DNS server
-  return newZone;
-}
+let zone = new Zone();
 
 router.route('/deth/v1/*')
   .get(function(req, res, next) {
-    let output = generateOutput(cachedZone);
+    let output = zone.generateOutput;
     res.json(output);
   })
   .post(jsonParser, function(req, res, next) {
     if (!req.body) { return next(new errors.BadRequestError()); }
 
     let newRecord = req.body;
-    let result = modifyZone(cachedZone, newRecord);
-    let output = generateOutput(result);
+    zone.modify(newRecord);
 
     res.status(201);
-    res.json(output);
+    res.json(zone.generateOutput);
   })
   .delete(jsonParser, function(req, res, next) {
     if (!req.body) { return next(new errors.BadRequestError()); }
 
     let removedRecord = req.body;
-    let result = modifyZone(cachedZone, removedRecord);
-    let output = generateOutput(result);
+    zone.modify(removedRecord);
 
     res.status(200);
-    res.json(output);
+    res.json(zone.generateOutput);
   });
